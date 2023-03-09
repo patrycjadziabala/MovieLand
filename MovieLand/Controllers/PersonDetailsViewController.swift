084ec0e5892dd734bdeb651e45bd5dd1d97321d5
Patrycja Dziabala <patrycja.dziabala@gmail.com>
2023-03-09 16:05:46 +0100

    Design PersonDetailsViewController and add scroll view

--- MovieLand/Controllers/PersonDetailsViewController.swift
+++ MovieLand/Controllers/PersonDetailsViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 class PersonDetailsViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var imageVieww: UIImageView!
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var roleLabel: UILabel!
+    @IBOutlet weak var birthDateLabel: UILabel!
+    @IBOutlet weak var personInfoTableView: UITextView!
+    @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var awardsLabel: UITextView!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var castMoviesLabel: UILabel!
+    @IBOutlet weak var movie1Image: UIImageView!
+    @IBOutlet weak var movie1TitleLabel: UILabel!
+    @IBOutlet weak var movie1RoleLabel: UILabel!
+    @IBOutlet weak var movie2Image: UIImageView!
+    @IBOutlet weak var movie2TitleLabel: UILabel!
+    @IBOutlet weak var movie2RoleLabel: UILabel!
+    @IBOutlet weak var movie3Image: UIImageView!
+    @IBOutlet weak var movie3TitleLabel: UILabel!
+    @IBOutlet weak var movie3RoleLabel: UILabel!
+    @IBOutlet weak var movie4Image: UIImageView!
+    @IBOutlet weak var movie4TitleLabel: UILabel!
+    @IBOutlet weak var movie4RoleLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
