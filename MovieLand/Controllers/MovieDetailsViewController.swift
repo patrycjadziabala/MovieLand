06acee7c5152eaaec7b0df247021cefe0c91fa13
Patrycja Dziabala <patrycja.dziabala@gmail.com>
2023-03-09 13:32:33 +0100

    Connect outlets to MovieDetailsViewController

--- MovieLand/Controllers/MovieDetailsViewController.swift
+++ MovieLand/Controllers/MovieDetailsViewController.swift
@@ -10,8 +10,33 @@ import UIKit
 class MovieDetailsViewController: UIViewController {
     
     @IBOutlet weak var movieTitleLabel: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var overviewTextView: UITextView!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    @IBOutlet weak var movieOverviewTextView: UITextView!
+    @IBOutlet weak var awardsTextView: UITextView!
+    @IBOutlet weak var castLabel: UILabel!
+    @IBOutlet weak var actor1Image: UIImageView!
+    @IBOutlet weak var actor1NameLabel: UILabel!
+    @IBOutlet weak var actor1AsCharacterLabel: UILabel!
+    @IBOutlet weak var actor2Image: UIImageView!
+    @IBOutlet weak var actor2NameLabel: UILabel!
+    @IBOutlet weak var actor2AsCharacterLabel: UILabel!
+    @IBOutlet weak var actor3Image: UIImageView!
+    @IBOutlet weak var actor3NameLabel: UILabel!
+    @IBOutlet weak var actor3AsCharacterLabel: UILabel!
+    @IBOutlet weak var actor4Image: UIImageView!
+    @IBOutlet weak var actor4NameLabel: UILabel!
+    @IBOutlet weak var actor4AsCharacterLabel: UILabel!
+    @IBOutlet weak var moreLikeThisLabel: UILabel!
+    @IBOutlet weak var movie1PosterImage: UIImageView!
+    @IBOutlet weak var movie1TitleLabel: UILabel!
+    @IBOutlet weak var movie2PosterImage: UIImageView!
+    @IBOutlet weak var movie2TitleLabel: UILabel!
+    @IBOutlet weak var movie3PosterImage: UIImageView!
+    @IBOutlet weak var movie3TitleLabel: UILabel!
+    @IBOutlet weak var movie4PosterImage: UIImageView!
+    @IBOutlet weak var movie4TitleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
